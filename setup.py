af1eb4c2307a3429e9af57978d8c5f2d6d314cf3
Narottam Sharma <narottamk077@gmail.com>
2024-09-01 18:37:45 +0530

    Added the module with loacal file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Narottam Sharma",
+    author_email="narottamaiengineer@gmail.com",
+    packages=find_packages()
+)
