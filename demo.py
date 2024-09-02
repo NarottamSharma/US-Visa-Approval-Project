e92ea7b1cfeb7c4daa11d189d5d431d8f83b17e9
Narottam Sharma <narottamk077@gmail.com>
2024-09-02 19:11:59 +0530

    Added configuraton

--- demo.py
+++ demo.py
@@ -1,11 +1,5 @@
-from us_visa.logger import logging
+from us_visa.constants import *
 
-import sys
-from us_visa.exception import USvisaException
+print(DATABASE_NAME)  # US_VISA
 
-try:
-    a=1/'10'
-except Exception as e:
-    logging.info(e)
-    raise USvisaException(e,sys) from e
-    hi
+print(COLLECTION_NAME)  # visa_data
